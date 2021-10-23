3e45243412dd3e6359fce0a29641278a09199ee7
Systemad <dannesuke@gmail.com>
2021-10-24 01:43:19 +0200

    Initial move to .NET 6

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,57 +1,47 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Blazored.LocalStorage;
+using Dovecord.Client;
 using Dovecord.Client.Services;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 using Refit;
 
-namespace Dovecord.Client
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddHttpClient("Dovecord.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("Dovecord.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
             
             // TODO: Make a separate service
             // https://dovecord.azurewebsites.net/api for Azure web app
-            builder.Services.AddRefitClient<IChannelApi>()
-                .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-            builder.Services.AddRefitClient<IChatApi>()
-                .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-            builder.Services.AddRefitClient<IUserApi>()
-                .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddRefitClient<IChannelApi>()
+    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddRefitClient<IChatApi>()
+    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddRefitClient<IUserApi>()
+    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
             
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Dovecord.ServerAPI"));
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Dovecord.ServerAPI"));
 
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://danovas.onmicrosoft.com/89be5e10-1770-45d7-813a-d47242ae2163/API.Access");
-                options.ProviderOptions.LoginMode = "redirect";
-            });
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://danovas.onmicrosoft.com/89be5e10-1770-45d7-813a-d47242ae2163/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
+});
             
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,96 +1,61 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Linq;
 using Dovecord.Data;
-using Dovecord.Data.Services;
 using Dovecord.Server.Hubs;
 using Dovecord.Server.Services;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
-namespace Dovecord.Server
+var builder = WebApplication.CreateBuilder(args);
+const string CorsPolicy = nameof(CorsPolicy);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=..\\Data\\DovecordHQ.db")
+);
+builder.Services.AddApplicationServices();
+builder.Services.AddAppAuthentication(builder.Configuration);
+builder.Services.AddCors(options =>
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
+    options.AddPolicy(
+        name: CorsPolicy,
+        builder =>
         {
-            Configuration = configuration;
-        }
-        
-        const string CorsPolicy = nameof(CorsPolicy);
-        public IConfiguration Configuration { get; }
-        
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Data Source=DovecordHQ.db for Azure web app
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite("Data Source=..\\Data\\DovecordHQ.db")
-            );
-            services.AddApplicationServices();
-            services.AddAppAuthentication(Configuration);
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    name: CorsPolicy,
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader();
-                    });
-            });
-            services.RegisterSwagger();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+builder.Services.RegisterSwagger();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
             
-            services.AddSignalR(options => options.EnableDetailedErrors = true)
-                .AddMessagePackProtocol();
-        }
+builder.Services.AddSignalR(options => options.EnableDetailedErrors = true)
+    .AddMessagePackProtocol();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebAssemblyDebugging();
-                app.ConfigureSwagger();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseCors(CorsPolicy);
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-                endpoints.MapHub<ChatHub>("/chathub");
-                endpoints.MapFallbackToFile("index.html");
-            });
-        }
-    }
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.ConfigureSwagger();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors(CorsPolicy);
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToFile("index.html");
+
+app.Run();
