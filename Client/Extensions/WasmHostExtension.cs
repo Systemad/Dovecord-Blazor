10489877ba318078aad72d98c5435b9394857833
Systemad <dannesuke@gmail.com>
2021-10-25 20:52:51 +0200

    Client: Extract services from Program.cs to separate service builder

---
+++ Client/Extensions/WasmHostExtension.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Http;
+using Blazored.LocalStorage;
+using Dovecord.Client.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using Refit;
+
+namespace Dovecord.Client.Extensions;
+
+public static class WasmHostExtension
+{
+    public static void AddClientServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddHttpClient("Dovecord.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+        builder.Services.AddRefitClient<IChannelApi>()
+            .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+        builder.Services.AddRefitClient<IChatApi>()
+            .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+        builder.Services.AddRefitClient<IUserApi>()
+            .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Dovecord.ServerAPI"));
+
+        builder.Services.AddMsalAuthentication(options =>
+        {
+            builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+            options.ProviderOptions.DefaultAccessTokenScopes.Add("https://danovas.onmicrosoft.com/89be5e10-1770-45d7-813a-d47242ae2163/API.Access");
+            options.ProviderOptions.LoginMode = "redirect";
+        });
+            
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddMudServices();
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,46 +1,13 @@
-using System;
-using System.Net.Http;
-using Blazored.LocalStorage;
 using Dovecord.Client;
-using Dovecord.Client.Services;
+using Dovecord.Client.Extensions;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using MudBlazor.Services;
-using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Dovecord.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-            
-// TODO: Make a separate service
-builder.Services.AddRefitClient<IChannelApi>()
-    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-builder.Services.AddRefitClient<IChatApi>()
-    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-builder.Services.AddRefitClient<IUserApi>()
-    .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/api"); })
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-            
-
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Dovecord.ServerAPI"));
-
-builder.Services.AddMsalAuthentication(options =>
-{
-    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://danovas.onmicrosoft.com/89be5e10-1770-45d7-813a-d47242ae2163/API.Access");
-    options.ProviderOptions.LoginMode = "redirect";
-});
-            
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddMudServices();
+builder.AddClientServices();
 
 await builder.Build().RunAsync();
